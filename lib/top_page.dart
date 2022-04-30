d45c79351089344bddc32fad4af742d99340f945
yuhei_suzuki <yooopopo5@gmail.com>
2022-04-30 23:24:01 +0900

    天気表示のモック画面の作成

--- lib/top_page.dart
+++ lib/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/weather.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -8,11 +9,48 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
+  Weather currentWeather = Weather(
+    temperature: 15,
+    description: '晴れ',
+    temperatureMaximum: 18,
+    temperatureMinimum: 14,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('天気を表示'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 25),
+            ),
+            const Text(
+              '大阪市',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            Text(currentWeather.description ?? ""),
+            Text(currentWeather.temperature != null
+                ? '${currentWeather.temperature}°'
+                : ""),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text(currentWeather.temperatureMaximum != null
+                      ? '最高：${currentWeather.temperatureMaximum}°'
+                      : ""),
+                ),
+                Text(currentWeather.temperatureMinimum != null
+                    ? '最低：${currentWeather.temperatureMinimum}°'
+                    : ""),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
